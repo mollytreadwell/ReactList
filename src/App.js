36b98b6446801a7012747bed91c54ed288f935b8
Molly Treadwell <mhtreadwell718@gmail.com>
2023-05-26 11:36:10 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './styles.css';
+
+function App() {
+
+    let array = ["Do dishes"];
+
+    function addItem(){
+        let x = document.getElementById("arrayList");
+        array.push(document.getElementById("new-todo-input").value);
+        x.innerHTML = array.join('<br/><li>');
+    }
+
+    return (
+      <div>
+        <h1>To-Do</h1>
+        <form>
+          <h2>What Needs To Be Done</h2>
+          <input className="add" type="text" id="new-todo-input"/>
+            <input className="submit" type="button" onClick={addItem} value="Add Item"/>          
+        </form>
+
+        <h2 id="list-heading">List of Tasks</h2>
+        <ul
+          role="list"
+          aria-labelledby="list-heading">
+          <li>
+            <div className="list" id="arrayList">{array}</div>
+          </li>
+        </ul>
+        <img className="pic" height="auto" width="750px" src="banana.jpg" alt="banana"/>
+      </div>
+    )
+}
+
+export default App;
